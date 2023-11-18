95f2f99b59720337e953cdf329b32536c9736632
Karina Romanova <rct.romanovaKE@bsu.by>
2023-11-19 00:21:19 +0300

    fixed cheak

--- src/Formik.js
+++ src/Formik.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import * as yup from 'yup';
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { Modal } from './Modal';
@@ -20,73 +20,77 @@ export const RegistrationForm = () => {
   const [show, setShow] = useState(false);
   const [value, setValue] = useState('');
   return (
-    <Formik initialValues={
-      { userName: '' }
-    }
-      validationSchema={validationSchema}
-      onSubmit={(values) => {
-        console.log(values);
-        setValue(JSON.stringify(values, {},5))
-        setShow(true)
-      }}
-    >
-      <Form className='App__input-holder'>
-        <div className='App__input-item'>
-          <label >Имя пользователя</label>
-          < Field type='text' id='userName' name='userName'
-            validate={validationSchema} />
-          <ErrorMessage className='App__error' name='userName' component='div'></ErrorMessage>
-        </div>
-        <div className='App__input-item'>
-          <label >Электронная почта</label>
-          < Field type='email' id='email' name='email'
-            validate={validationSchema} />
-          <ErrorMessage className='App__error' name='email' component='div'></ErrorMessage>
-        </div>
-        <div className='App__input-item'>
-          <label >Пароль</label>
-          < Field type='password' id='password' name='password'
-            validate={validationSchema} />
-          <ErrorMessage className='App__error' name='password' component='div'></ErrorMessage>
-        </div>
-        <div className='App__input-item'>
-          <label >Подтверждение пароля</label>
-          < Field type='password' id='confirmPassword' name='confirmPassword'
-            validate={validationSchema} />
-          <ErrorMessage className='App__error' name='confirmPassword' component='div'></ErrorMessage>
-        </div>
-        <div className='App__input-item'>
-          <label >Дата рождения</label>
-          < Field type='date' id='dateOfBirth' name='dateOfBirth'
-            validate={validationSchema} />
-          <ErrorMessage className='App__error' name='dateOfBirth' component='div'></ErrorMessage>
-        </div>
-        <div className='App__input-item input-item'>
-          <label >Пол</label>
-          <div className='input-item__holder'>
-            <label className='input-item__gender'>
-              < Field type='radio' name='gender' value='мужской'
-                validate={validationSchema} />
-              Мужской
-            </label>
-            <label className='input-item__gender'>
-              < Field type='radio' name='gender' value='женский'
-                validate={validationSchema} />
-              Женский
-            </label>
+    <>
+      <Formik initialValues={
+        { userName: '' }
+      }
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          console.log(values);
+          setValue(JSON.stringify(values, {}, 5))
+          setShow(true)
+        }}
+      >
+        <Form className='App__input-holder'>
+          <div className='App__input-item'>
+            <label >Имя пользователя</label>
+            < Field type='text' id='userName' name='userName'
+              validate={validationSchema} />
+            <ErrorMessage className='App__error' name='userName' component='div'></ErrorMessage>
           </div>
-          <ErrorMessage className='App__error' name='gender' component='div'></ErrorMessage>
-        </div>
-        <div className='App__input-item'>
-          <label >Telephone</label>
-          < Field type='tel' id='phone' name='phone'
-            validate={validationSchema} />
-          <ErrorMessage className='App__error' name='phone' component='div'></ErrorMessage>
-        </div>
-        <Modal show={show} value={value} onClose={()=>setShow(false) } />
-        <button type='submit'>зарегистрироваться</button>
-      </Form>
-    </Formik>
+          <div className='App__input-item'>
+            <label >Электронная почта</label>
+            < Field type='email' id='email' name='email'
+              validate={validationSchema} />
+            <ErrorMessage className='App__error' name='email' component='div'></ErrorMessage>
+          </div>
+          <div className='App__input-item'>
+            <label >Пароль</label>
+            < Field type='password' id='password' name='password'
+              validate={validationSchema} />
+            <ErrorMessage className='App__error' name='password' component='div'></ErrorMessage>
+          </div>
+          <div className='App__input-item'>
+            <label >Подтверждение пароля</label>
+            < Field type='password' id='confirmPassword' name='confirmPassword'
+              validate={validationSchema} />
+            <ErrorMessage className='App__error' name='confirmPassword' component='div'></ErrorMessage>
+          </div>
+          <div className='App__input-item'>
+            <label >Дата рождения</label>
+            < Field type='date' id='dateOfBirth' name='dateOfBirth'
+              validate={validationSchema} />
+            <ErrorMessage className='App__error' name='dateOfBirth' component='div'></ErrorMessage>
+          </div>
+          <div className='App__input-item input-item'>
+            <label >Пол</label>
+            <div className='input-item__holder'>
+              <label className='input-item__gender'>
+                < Field type='radio' name='gender' value='мужской'
+                  validate={validationSchema} />
+                Мужской
+              </label>
+              <label className='input-item__gender'>
+                < Field type='radio' name='gender' value='женский'
+                  validate={validationSchema} />
+                Женский
+              </label>
+            </div>
+            <ErrorMessage className='App__error' name='gender' component='div'></ErrorMessage>
+          </div>
+          <div className='App__input-item'>
+            <label >Telephone</label>
+            < Field type='tel' id='phone' name='phone'
+              validate={validationSchema} />
+            <ErrorMessage className='App__error' name='phone' component='div'></ErrorMessage>
+          </div>
+          <button type='submit'>зарегистрироваться</button>
+        </Form>
+      </Formik>
+      {
+        !show ? null : <Modal value={value} onClose={() => setShow(false)} />
+      }
+    </>
 
 
     // <Formik initialValues={{ username: '', email: '', password: '', confirmPassword: '', data: '', MW: '', telephone: '' }}
--- src/Modal.js
+++ src/Modal.js
@@ -1,13 +1,10 @@
-export const Modal = ({ show, onClose, value }) => {
-    if (!show) { 
-        return null
-    }
+export const Modal = ({onClose, value }) => {
     return (
         <div className="modal">
             <div className="modal__container">
                 <div className="modal__content">
                     <div className="modal__close">
-                        <div onClick={onClose} className="modal__button"><img  src={require('./icon/cross.svg').default} /></div>
+                        <div onClick={onClose} className="modal__button"><img src={require('./icon/cross.svg').default} /></div>
                     </div>
                     <pre className="modal__info"><p>{value}</p></pre>
                     <div className="modal__message">Успешно зарегистрированно</div>
